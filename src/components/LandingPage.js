5253ef00762f986e5a52ef15b6c6889f6b218140
Gabi <86157167+GabrielPetrov@users.noreply.github.com>
2024-03-15 00:25:21 +0200

    Landing page routing, search button

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import "./LandingPage.css";
 
-const LandingPage = () => {
+const LandingPage = () => { 
+    const navigate = useNavigate();
+    const goToMap = () => {
+        navigate('/map');
+    }
     return (
         <div>
             <h1>Welcome to our app!</h1>
-            <button>to map</button>
+            <button onClick={goToMap} className="button">Go to Map</button>
         </div>
     );
 }
