79a7096bb7488332b34f504760217fd4d2926be6
Gabi <86157167+GabrielPetrov@users.noreply.github.com>
2024-03-15 01:59:03 +0200

    Path back to landing page, attempts at vector art for fish markers

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import SearchBar from './SearchBar';
 import './Menu.css';
 
 const Menu = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(true);
+  const navigate = useNavigate();
   const handleClick = () => {
-    window.location.reload();
+    navigate('/');
   };
   const toggleMenu = () => {
       setIsMenuOpen(!isMenuOpen);
