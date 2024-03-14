2ac46b68436742b063dbd5b9a687b35f5302b76c
Nikieprogramach <nikola.aleksov8@gmail.com>
2024-03-14 11:59:27 +0200

    added tooltip to show hoover country

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,4 @@
-import React from "react";
-
+import React, { useState } from "react";
 import {
  ComposableMap,
  Geographies,
@@ -8,6 +7,10 @@ import {
  Annotation,
  ZoomableGroup
 } from "react-simple-maps"
+
+import {Tooltip as ReactTooltip} from "react-tooltip"
+import 'react-tooltip/dist/react-tooltip.css'
+
 import MapMarker from "./MapMarker";
 
 const geoUrl = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json"
@@ -20,32 +23,35 @@ const Markers = [
 ]
 
 function Map() {
-  return (
-    <div style={{width: "100%", height: "100%", display: "flex"}}>
-        <p>Map</p>
-        <div style={{width: "1400px"}}>
-            <ComposableMap>
-                <ZoomableGroup zoom={1}>
-                    <Geographies geography="/map.json">
-                        {({ geographies }) =>
-                        geographies.map((geo) => (
-                            <Geography key={geo.rsmKey} geography={geo} />
-                        ))
-                        }
-                    </Geographies>
-                    {
-                        Markers.map(({name, coordinates}) => (
-                            <Marker key={name} coordinates={coordinates}>
-                                <circle r={5} fill="#F00" strokeWidth={2}/>
-                                <h1>{name}</h1>
-                            </Marker>
-                        ))
-                    }                    
-                </ZoomableGroup>
-            </ComposableMap>
+    const [content, setContent] = useState("")
+    return (
+        <div style={{width: "100%", height: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+            <p>Map</p>
+            <ReactTooltip id="tooltip" place="top" content={content}/>
+            <div style={{width: "90vw"}}>
+                <ComposableMap data-tip="" style={{height: "80vh", width: "90vw"}}>
+                    <ZoomableGroup zoom={1}>
+                        {" "}
+                        <Geographies geography="/map.json">
+                            {({ geographies }) =>
+                            geographies.map((geo) => (
+                                <Geography key={geo.rsmKey} geography={geo} data-tooltip-id = "tooltip" onMouseEnter={() => {setContent(geo.properties.name); console.log(content)}} onMouseLeave={() => {setContent("")}} style={{hover: {fill: "red", outline: "none"}}}/>
+                            ))
+                            }
+                        </Geographies>
+                        {
+                            Markers.map(({name, coordinates}) => (
+                                <Marker key={name} coordinates={coordinates}>
+                                    <circle r={1} fill="#F00" strokeWidth={2}/>
+                                    <h1>{name}</h1>
+                                </Marker>
+                            ))
+                        }                 
+                    </ZoomableGroup>
+                </ComposableMap>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default Map;
